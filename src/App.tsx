af844df4a020b75e218b2f6a8be4f8a01b6432a9
acu8 <ewatanabe786@gmail.com>
2024-08-19 03:39:20 +0900

    route done

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
     <>
       <UserProvider>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/newuser" element={<NewUserPage />} />
           <Route element={<ProtectedRoute />}>
